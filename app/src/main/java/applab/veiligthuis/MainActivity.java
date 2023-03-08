97dfabee187728b8ebfc27ab142849a3ca91a21e
Dennisgog87 <dennisgog@gmail.com>
2023-03-08 20:25:54 +0100

    Initiële code, documentatie verplaatst naar aparte folder.

---
+++ app/src/main/java/applab/veiligthuis/MainActivity.java
@@ -0,0 +1,14 @@
+package applab.veiligthuis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
