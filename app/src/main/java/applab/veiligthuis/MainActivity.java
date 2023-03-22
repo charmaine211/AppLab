5c83098a71c2fd4db9668c8a919c08ba3483ce5a
jey-ou <83501213+jey-ou@users.noreply.github.com>
2023-03-22 16:43:33 +0100

    Aanpassingen aan de developper branch om de app te kunnen runnen

--- app/src/main/java/applab/veiligthuis/MainActivity.java
+++ app/src/main/java/applab/veiligthuis/MainActivity.java
@@ -23,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
 
         // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        FirebaseDatabase database;
+        database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
 
         myRef.setValue("Hello, World!");
