9ac5bfaf6e3c90ae2f7fa74aae2e834bce15ebca
jey-ou <83501213+jey-ou@users.noreply.github.com>
2023-04-23 20:22:42 +0200

    aanpassingen om toolbar voor home en login mogelijk te maken

--- app/src/main/java/applab/veiligthuis/MainActivity.java
+++ app/src/main/java/applab/veiligthuis/MainActivity.java
@@ -7,11 +7,14 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.firebase.database.FirebaseDatabase;
 
 import applab.veiligthuis.activity.MeldingenActivity;
+import applab.veiligthuis.activity.SignInUp.LogInActivity;
 import applab.veiligthuis.activity.tip.TipBeheren;
 import applab.veiligthuis.activity.tip.TipInzien;
 
@@ -23,11 +26,26 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+
+
         initMeldingenButton();
         initTipsInzienButton();
         initTipsBeherenButton();
 
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+
+        Toolbar toolbar = findViewById(R.id.veilig_thuis_toolbar);
+        setSupportActionBar(toolbar);
+        ImageView imageView_tb = findViewById(R.id.second_image_view);
+
+        imageView_tb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,
+                        LogInActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
