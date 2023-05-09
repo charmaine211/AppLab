595756a7dfeb1922e5aa2aa774908ab6450b1fb6
@milcokats <milcokats@gmail.com>
2023-05-09 19:40:02 +0200

    Firebase authenticatie voor login geimplementeerd

--- app/src/main/java/applab/veiligthuis/activity/SignInUp/LogInActivity.java
+++ app/src/main/java/applab/veiligthuis/activity/SignInUp/LogInActivity.java
@@ -1,10 +1,14 @@
 package applab.veiligthuis.activity.SignInUp;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +16,12 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import applab.veiligthuis.MainActivity;
 import applab.veiligthuis.R;
@@ -22,6 +32,7 @@ public class LogInActivity extends AppCompatActivity {
     TextView textViewForgotPassWord;
     TextView textViewRegister;
     Button btnSignIn;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +44,7 @@ public class LogInActivity extends AppCompatActivity {
         textViewForgotPassWord = findViewById(R.id.textViewForgetPassword);
         textViewRegister = findViewById(R.id.textViewRegister);
         btnSignIn = findViewById(R.id.buttonLogIn);
+        mAuth = FirebaseAuth.getInstance();
 
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,15 +95,35 @@ public class LogInActivity extends AppCompatActivity {
         String userName = editTextUserName.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
 
-        if(!Patterns.EMAIL_ADDRESS.matcher(userName).matches()){
+        if(userName.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(userName).matches()){
             editTextUserName.setError("Please enter valid email address");
             editTextUserName.requestFocus();
         }
-        if(editTextPassword.length()<7){
+        if(password.isEmpty() || editTextPassword.length()<7){
             editTextPassword.setError("Please enter password at least 7 characters");
             editTextPassword.requestFocus();
         }
 
-        Toast.makeText(getApplicationContext(),"optie nog niet voledig geïmplementeerd", Toast.LENGTH_SHORT).show();
-    }
+        else{
+            mAuth.signInWithEmailAndPassword(userName, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Toast.makeText(getApplicationContext(), "Authentication Successful.",
+                                        Toast.LENGTH_SHORT).show();
+                                finish();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+            }
+        }
 }
