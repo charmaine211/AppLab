182cc388641b80c01ab10ac081cda2f5edd7ed10
Milco Kats <milcokats@gmail.com>
2023-05-20 21:11:21 +0200

    WIP

--- app/src/main/java/applab/veiligthuis/activity/meldingen/MeldingMakenFragment.java
+++ app/src/main/java/applab/veiligthuis/activity/meldingen/MeldingMakenFragment.java
@@ -7,9 +7,17 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.time.LocalDateTime;
 
@@ -23,7 +31,6 @@ public class MeldingMakenFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -51,25 +58,44 @@ public class MeldingMakenFragment extends Fragment {
 
     public void initSaveButton(){
 
-        // Get reference to EditText
         final EditText meldingEditText = getView().findViewById(R.id.meldingmaken_editTextTextMultiLine);
 
-        // Save button click listener
         getView().findViewById(R.id.save_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                // Get values from spinner and EditText
                 String plaatsnaam = ((Spinner) getView().findViewById(R.id.plaatsnaam_spinner)).getSelectedItem().toString();
 
                 String beschrijving = meldingEditText.getText().toString().trim();
 
                 LocalDateTime datum = LocalDateTime.now();
 
-                // Save melding in ViewModel
+                FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+                if (currentUser != null) {
+                    String uid = currentUser.getUid();
+                    meldingViewModel.insertMelding(plaatsnaam, beschrijving, datum.toString(), uid);
+                } else {
+                    FirebaseAuth.getInstance().signInAnonymously()
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+
+                                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                        String uid = user.getUid();
+
+                                        meldingViewModel.insertMelding(plaatsnaam, beschrijving, datum.toString(), uid);
+
+                                        meldingEditText.setText("");
+                                    } else {
+                                        Toast.makeText(getActivity(), "Error creating anonymous user", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
                 meldingViewModel.insertMelding(plaatsnaam, beschrijving, datum.toString());
 
-                // Clear EditText
                 meldingEditText.setText("");
             }
         });
