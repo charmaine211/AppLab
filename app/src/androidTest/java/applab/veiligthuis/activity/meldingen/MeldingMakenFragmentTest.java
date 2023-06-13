178b9cb5cef5241e6dd1ede9b4d52326ef9e438f
CharmaineN <charmaine211@gmail.com>
2023-06-13 16:58:58 +0200

    Ik moet de tests nog testen en uitzoeken hoe ik de tekst van een toast kan controleren.

--- app/src/androidTest/java/applab/veiligthuis/activity/meldingen/MeldingMakenFragmentTest.java
+++ app/src/androidTest/java/applab/veiligthuis/activity/meldingen/MeldingMakenFragmentTest.java
@@ -3,26 +3,52 @@ package applab.veiligthuis.activity.meldingen;
 import androidx.fragment.app.testing.FragmentScenario;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.matcher.IntentMatchers;
+import androidx.test.espresso.matcher.RootMatchers;
 import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import applab.veiligthuis.R;
+import applab.veiligthuis.activity.home.MainActivity;
+
 import junit.framework.TestCase;
 
 import org.hamcrest.Matchers;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 public class MeldingMakenFragmentTest extends TestCase {
     FragmentScenario<MeldingMakenFragment> scenario;
 
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule;
+
     public void setUp(){
         scenario = FragmentScenario.launchInContainer(MeldingMakenFragment.class);
+        activityRule = new ActivityScenarioRule<>(MainActivity.class);
+    }
+
+    @Before
+    public void setUpIntent(){
+        Intents.init();
+    }
+
+    @After
+    public void cleanup() {
+        Intents.release();
     }
 
     @Test
@@ -42,14 +68,15 @@ public class MeldingMakenFragmentTest extends TestCase {
         onView(ViewMatchers.isAssignableFrom(RecyclerView.class))
                 .perform(RecyclerViewActions.scrollToPosition(0))
                 .check(matches(withSpinnerText(Matchers.not(""))));
-
-
     }
 
     @Test
     public void testMaakMelding(){
-
+        onView(withId(R.id.meldingmaken_editTextTextMultiLine)).perform(typeText(""), closeSoftKeyboard());
+        onView(withId(R.id.opslaan_button)).check(matches(isDisplayed())).perform(click());
+        //TODO: Test of de toast wordt getoond
+        onView(withId(R.id.meldingmaken_editTextTextMultiLine)).perform(typeText("Ik wil een melding maken van..."), closeSoftKeyboard());
+        onView(withId(R.id.opslaan_button)).check(matches(isDisplayed())).perform(click());
+        Intents.intended(IntentMatchers.hasComponent(MainActivity.class.getName()));
     }
-
-
 }
--- app/src/androidTest/java/applab/veiligthuis/activity/meldingen/RisicoAnalyseFragmentTest.java
+++ app/src/androidTest/java/applab/veiligthuis/activity/meldingen/RisicoAnalyseFragmentTest.java
@@ -1,25 +1,51 @@
 package applab.veiligthuis.activity.meldingen;
 
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.matcher.IntentMatchers;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+
+import android.content.Intent;
+
 import applab.veiligthuis.R;
+import applab.veiligthuis.activity.home.MainActivity;
 
 import androidx.fragment.app.testing.FragmentScenario;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import junit.framework.TestCase;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 public class RisicoAnalyseFragmentTest extends TestCase {
 
     FragmentScenario<RisicoAnalyseFragment> scenario;
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule;
 
     public void setUp(){
         scenario = FragmentScenario.launchInContainer(RisicoAnalyseFragment.class);
+        activityRule = new ActivityScenarioRule<>(MainActivity.class);
+    }
+
+    @Before
+    public void setUpIntent(){
+        Intents.init();
+    }
+
+    @After
+    public void cleanup() {
+        Intents.release();
     }
 
     @Test
@@ -30,24 +56,42 @@ public class RisicoAnalyseFragmentTest extends TestCase {
     }
 
     @Test
-    public void testJaButton() {
+    public void testJaButtonPos() {
         onView(withId(R.id.ja_button)).perform(click());
-        onView(withId(android.R.id.button1)).perform(click());
-        // Voeg hier eventuele assertions toe om te controleren of de telefoonapp is gestart
+        onView(withText("We raden u aan om 112 te bellen.")).check(matches(isDisplayed()));
+        onView(withId(android.R.id.button1)).perform(click()); // PositiveButton
+        Intents.intended(IntentMatchers.hasAction(Intent.ACTION_DIAL));
+    }
+
+    @Test
+    public void testJaButtonNeg() {
+        onView(withId(R.id.ja_button)).perform(click());
+        onView(withText("We raden u aan om 112 te bellen.")).check(matches(isDisplayed()));
+        onView(withId(android.R.id.button2)).perform(click()); // NegativeButton
+        onView(withText("We raden u aan om 112 te bellen.")).check(doesNotExist());
+    }
+
+    @Test
+    public void testTwijfelButtonPos() {
+        onView(withId(R.id.twijfel_button)).perform(click());
+        onView(withText("We raden u aan ons te bellen.")).check(matches(isDisplayed()));
+        onView(withId(android.R.id.button1)).perform(click()); // PositiveButton
+        Intents.intended(IntentMatchers.hasAction(Intent.ACTION_DIAL));
+
+    }
+
+    @Test
+    public void testTwijfelButtonNeg() {
+        onView(withId(R.id.twijfel_button)).perform(click());
+        onView(withText("We raden u aan ons te bellen.")).check(matches(isDisplayed()));
+        onView(withId(android.R.id.button2)).perform(click()); // NegativeButton
+        onView(withText("We raden u aan ons te bellen.")).check(doesNotExist());
     }
 
     @Test
     public void testNeeButton() {
         onView(withId(R.id.nee_button)).perform(click());
-        onView(withId(android.R.id.button1)).perform(click());
-        // Voeg hier eventuele assertions toe om te controleren of de telefoonapp is gestart
-    }
-
-    @Test
-    public void testTwijfelButton() {
-        onView(withId(R.id.twijfel_button)).perform(click());
-        onView(withId(android.R.id.button1)).perform(click());
-        // Voeg hier eventuele assertions toe om te controleren of de telefoonapp is gestart
+        Intents.intended(IntentMatchers.hasComponent(MeldingMakenActivity.class.getName()));
     }
 
 
