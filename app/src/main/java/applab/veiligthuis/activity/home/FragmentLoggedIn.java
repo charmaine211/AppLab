9676749be01b3dc7f4ad04babd113310a2f37cf2
CharmaineN <charmaine211@gmail.com>
2023-06-27 14:47:37 +0200

    De knop Tips is verwijderd van het scherm van de zorgverlener. Dit omdat het niet nodig is voor een zorgverlener om nog extra de tips in te zien. Dit kan namelijk al onder tips beheren.

--- app/src/main/java/applab/veiligthuis/activity/home/FragmentLoggedIn.java
+++ app/src/main/java/applab/veiligthuis/activity/home/FragmentLoggedIn.java
@@ -23,7 +23,6 @@ public class FragmentLoggedIn extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_logged_in, container, false);
         initMeldingenButton(view);
-        initTipsInzienButton(view);
         initTipsBeherenButton(view);
         initMaakMeldingButton(view);
         return view;
@@ -46,17 +45,6 @@ public class FragmentLoggedIn extends Fragment {
             @Override
             public void onClick(View view) {
                 Intent tipsIntent = new Intent(requireActivity(), TipBeheren.class);
-                startActivity(tipsIntent);
-            }
-        });
-    }
-
-    public void initTipsInzienButton(View view) {
-        Button tipsInzienBtn = view.findViewById(R.id.tipsInzienButton);
-        tipsInzienBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent tipsIntent = new Intent(requireActivity(), TipInzien.class);
                 startActivity(tipsIntent);
             }
         });
