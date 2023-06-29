b7c6a968688fa17040b60ad8d826e6497db1b456
CharmaineN <charmaine211@gmail.com>
2023-06-29 13:05:09 +0200

    Ik heb de bijlage tekst een lege string gemaakt. Ook heb ik een constraint geplaatst onder de tekst het risico analyse fragment: "verkeert u, of iemand in uw omgeving..." De tekst is goed zichtbaar in mijn emulator. Maar ik heb een extra expressotest toegevoegd waarin de zichtbaarheid van de tekst gecontroleerd wordt.

--- app/src/androidTest/java/applab/veiligthuis/activity/meldingen/RisicoAnalyseFragmentTest.java
+++ app/src/androidTest/java/applab/veiligthuis/activity/meldingen/RisicoAnalyseFragmentTest.java
@@ -7,7 +7,9 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
@@ -17,6 +19,7 @@ import android.content.Intent;
 import applab.veiligthuis.R;
 
 import androidx.fragment.app.testing.FragmentScenario;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -53,70 +56,55 @@ public class RisicoAnalyseFragmentTest extends TestCase {
 
     @Test
     public void testKnopZichtbaar() {
-        try{
-            onView(withId(R.id.ja_button)).check(matches(isDisplayed()));
-            onView(withId(R.id.twijfel_button)).check(matches(isDisplayed()));
-            onView(withId(R.id.nee_button)).check(matches(isDisplayed()));
-        } catch (Exception e){
-            fail("Foutmelding: " + e.toString());
-        }
+        onView(withId(R.id.ja_button)).check(matches(isDisplayed()));
+        onView(withId(R.id.twijfel_button)).check(matches(isDisplayed()));
+        onView(withId(R.id.nee_button)).check(matches(isDisplayed()));
+
+    }
+
+    @Test
+    public void testTextViewZichtbaar() {
+        onView(withId(R.id.risicoAnalyse_textView))
+                .check(matches(isDisplayed()))
+                .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+                .check(matches(isCompletelyDisplayed()));
     }
 
     @Test
     public void testJaButtonPos() {
-        try{
-            onView(withId(R.id.ja_button)).perform(click());
-            onView(withText("We raden u aan om 112 te bellen.")).check(matches(isDisplayed()));
-            onView(withId(android.R.id.button1)).perform(click()); // PositiveButton
-            Intents.intended(IntentMatchers.hasAction(Intent.ACTION_DIAL));
-        } catch (Exception e){
-            fail("Foutmelding: " + e.toString());
-        }
+        onView(withId(R.id.ja_button)).perform(click());
+        onView(withText("We raden u aan om 112 te bellen.")).check(matches(isDisplayed()));
+        onView(withId(android.R.id.button1)).perform(click()); // PositiveButton
+        Intents.intended(IntentMatchers.hasAction(Intent.ACTION_DIAL));
     }
 
     @Test
     public void testJaButtonNeg() {
-        try{
-            onView(withId(R.id.ja_button)).perform(click());
-            onView(withText("We raden u aan om 112 te bellen.")).check(matches(isDisplayed()));
-            onView(withId(android.R.id.button2)).perform(click()); // NegativeButton
-            onView(withText("We raden u aan om 112 te bellen.")).check(doesNotExist());
-        } catch (Exception e){
-            fail("Foutmelding: " + e.toString());
-        }
+        onView(withId(R.id.ja_button)).perform(click());
+        onView(withText("We raden u aan om 112 te bellen.")).check(matches(isDisplayed()));
+        onView(withId(android.R.id.button2)).perform(click()); // NegativeButton
+        onView(withText("We raden u aan om 112 te bellen.")).check(doesNotExist());
     }
 
     @Test
     public void testTwijfelButtonPos() {
-        try{
-            onView(withId(R.id.twijfel_button)).perform(click());
-            onView(withText("We raden u aan ons te bellen.")).check(matches(isDisplayed()));
-            onView(withId(android.R.id.button1)).perform(click()); // PositiveButton
-            Intents.intended(IntentMatchers.hasAction(Intent.ACTION_DIAL));
-        } catch (Exception e){
-            fail("Foutmelding: " + e.toString());
-        }
+        onView(withId(R.id.twijfel_button)).perform(click());
+        onView(withText("We raden u aan ons te bellen.")).check(matches(isDisplayed()));
+        onView(withId(android.R.id.button1)).perform(click()); // PositiveButton
+        Intents.intended(IntentMatchers.hasAction(Intent.ACTION_DIAL));
     }
 
     @Test
     public void testTwijfelButtonNeg() {
-        try{
-            onView(withId(R.id.twijfel_button)).perform(click());
-            onView(withText("We raden u aan ons te bellen.")).check(matches(isDisplayed()));
-            onView(withId(android.R.id.button2)).perform(click()); // NegativeButton
-            onView(withText("We raden u aan ons te bellen.")).check(doesNotExist());
-        } catch (Exception e){
-            fail("Foutmelding: " + e.toString());
-        }
+        onView(withId(R.id.twijfel_button)).perform(click());
+        onView(withText("We raden u aan ons te bellen.")).check(matches(isDisplayed()));
+        onView(withId(android.R.id.button2)).perform(click()); // NegativeButton
+        onView(withText("We raden u aan ons te bellen.")).check(doesNotExist());
     }
 
     @Test
-    public void testNeeButton() {
-        try{
-            onView(withId(R.id.nee_button)).perform(click());
-            Intents.intended(IntentMatchers.hasComponent(MeldingMakenActivity.class.getName()));
-        } catch (Exception e){
-            fail("Foutmelding: " + e.toString());
-        }
+public void testNeeButton() {
+        onView(withId(R.id.nee_button)).perform(click());
+        Intents.intended(IntentMatchers.hasComponent(MeldingMakenActivity.class.getName()));
     }
 }
