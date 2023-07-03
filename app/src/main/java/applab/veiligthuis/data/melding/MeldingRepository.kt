ebd8e1d1dc309537ff9a3758c73832bc9404de07
Dennisgog87 <dennisgog@gmail.com>
2023-07-03 17:29:17 +0200

    Refactor van repository

--- app/src/main/java/applab/veiligthuis/data/melding/MeldingRepository.kt
+++ app/src/main/java/applab/veiligthuis/data/melding/MeldingRepository.kt
@@ -7,7 +7,8 @@ import applab.veiligthuis.domain.util.MeldingType
 import kotlinx.coroutines.flow.Flow
 
 interface MeldingRepository {
-    fun getMeldingen(paths: List<String>, meldingType: MeldingType): Flow<List<Melding?>>
+    fun getMeldingen(meldingType: MeldingType): Flow<List<Melding?>>
+    fun getMeldingenPlaatsen(plaatsen: List<String>, meldingType: MeldingType): Flow<List<Melding?>>
     fun getMelding(meldingKey: String, meldingType: MeldingType): Flow<Melding>
 
     @Throws(MeldingInsertException::class)
