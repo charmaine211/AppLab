dcba23294764fa3e9528bb5655c202f87dbca9f0
Dennisgog87 <dennisgog@gmail.com>
2023-07-03 14:24:48 +0200

    Refactor, Knop bij filterscherm toegevoegd.

--- app/src/main/java/applab/veiligthuis/views/meldinglist/FilterScreen.kt
+++ app/src/main/java/applab/veiligthuis/views/meldinglist/FilterScreen.kt
@@ -1,26 +1,31 @@
 package applab.veiligthuis.views.meldinglist
 
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Divider
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import applab.veiligthuis.R
 import applab.veiligthuis.ui.composable.filter.FilterCheckBoxList
 import applab.veiligthuis.ui.composable.filter.FilterHeader
 import applab.veiligthuis.ui.composable.filter.FilterRadioButtonList
+import applab.veiligthuis.ui.theme.AppTheme
+import applab.veiligthuis.ui.theme.filter_blue
+import applab.veiligthuis.ui.theme.veilig_thuis_oranje
 import applab.veiligthuis.views.Screens
 
 
@@ -37,13 +42,15 @@ fun FilterScreen(
                 rememberScrollState()
             )
     ) {
-
         FilterHeader(
             filterCount = filterState.filterCountSelected,
             headerTitle = stringResource(R.string.filter_header_title),
             closeFilter = {
                 onEvent(MeldingLijstEvent.SluitFilter)
-                navController.popBackStack(route = Screens.MeldingLijst.route, inclusive = false)
+                navController.popBackStack(
+                    route = Screens.MeldingLijst.route,
+                    inclusive = false
+                )
             },
             modifier = Modifier.padding(bottom = 18.dp)
         )
@@ -112,10 +119,61 @@ fun FilterScreen(
                 )
             }
         )
+        if (filterState.filterCountSelected > 0) {
+            Box(modifier = Modifier.height(100.dp))
+        }
     }
+    if (filterState.filterCountSelected > 0) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight(0.8F)
+                    .fillMaxWidth()
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(),
+                contentAlignment = Alignment.Center
+            ) {
+                Button(
+                    onClick = {
+                        onEvent(MeldingLijstEvent.SluitFilter)
+                        navController.popBackStack(
+                            route = Screens.MeldingLijst.route,
+                            inclusive = false
+                        )
+                    },
+                    colors = ButtonDefaults.buttonColors(backgroundColor = veilig_thuis_oranje),
+                    elevation = ButtonDefaults.elevation(defaultElevation = 10.dp),
+                    modifier = Modifier.padding(top = 20.dp).width(250.dp).height(55.dp)
+
+                ) {
+                    Text(
+                        text = stringResource(R.string.filter_pas_toe),
+                        color = Color.White
+                    )
+                }
+            }
+        }
+    }
+
 }
 
-
+@Preview(showBackground = true)
+@Composable
+fun PreviewFilterScreen() {
+    AppTheme {
+        FilterScreen(
+            navController = rememberNavController(),
+            filterState = MeldingLijstFilterState(),
+            onEvent = {})
+    }
+}
 
 
 
